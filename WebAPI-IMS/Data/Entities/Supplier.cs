eefe30d5a42374944e6cab01ab3189faacbc8b04
Devang Khatri <devang.khatri@desuvit.com>
2024-11-22 16:46:04 +0530

    Setting up entities

--- WebAPI-IMS/Data/Entities/Supplier.cs
+++ WebAPI-IMS/Data/Entities/Supplier.cs
@@ -2,5 +2,9 @@
 {
     public class Supplier
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }
